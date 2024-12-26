2f482b9f884e135b4e9e83cb39e32f81a24568bf
eycewind <hassaanshakil@gmail.com>
2024-12-26 21:44:43 +0500

    creating bot for trade automation

--- BinanceClient.py
+++ BinanceClient.py
@@ -5,13 +5,18 @@ import time
 import datetime
 
 class BinanceClient:
-    def __init__(self, api_key, api_secret, db_name="binance_data.db"):
+    def __init__(self, db_name="binance_data.db"):
+        """
+        Initialize the BinanceClient database name.
+        """
+        self.db_name = db_name
+        self.create_db()
+
+    def make(self, api_key, api_secret):
         """
         Initialize the BinanceClient with API credentials and database name.
         """
         self.client = Client(api_key, api_secret)
-        self.db_name = db_name
-        self.create_db()
 
     def create_db(self):
         """
@@ -103,9 +108,92 @@ class BinanceClient:
         try:
             candles = self.client.get_historical_klines(pair, interval, start_date, end_date)
             # Store the data in the database for future use
-            self.store_data_to_db(pair, candles)
+            # self.store_data_to_db(pair, candles)
             print("Fetching data from Binance API...")
-            return candles
+            df = pd.DataFrame(candles, columns=['timestamp', 'open', 'high', 'low', 'close', 'volume', 'close_time', 'quote_asset_value', 'number_of_trades', 'taker_buy_base_asset_volume', 'taker_buy_quote_asse_volume', 'Ignore'])
+            df.drop(['timestamp', 'open', 'high', 'low', 'close', 'volume'], axis=1)
+            return df
         except Exception as e:
             print(f"Error fetching data from Binance: {e}")
             return None
+
+    def calculate_sma(self, df):
+        """
+        Calculate Simple Moving Averages (SMA).
+        """
+        df['sma_10'] = df['close'].rolling(window=10).mean()
+        df['sma_50'] = df['close'].rolling(window=50).mean()
+
+    def calculate_ema(self, df):
+        """
+        Calculate Exponential Moving Averages (EMA).
+        """
+        df['ema_10'] = df['close'].ewm(span=10, adjust=False).mean()
+
+    def calculate_rsi(self, df):
+        """
+        Calculate Relative Strength Index (RSI).
+        """
+        delta = df['close'].diff()
+        gain = (delta.where(delta > 0, 0)).rolling(window=14).mean()
+        loss = (-delta.where(delta < 0, 0)).rolling(window=14).mean()
+        rs = gain / loss
+        df['rsi_14'] = 100 - (100 / (1 + rs))
+
+    def calculate_macd(self, df):
+        """
+        Calculate Moving Average Convergence Divergence (MACD).
+        """
+        ema_12 = df['close'].ewm(span=12, adjust=False).mean()
+        ema_26 = df['close'].ewm(span=26, adjust=False).mean()
+        df['macd'] = ema_12 - ema_26
+        df['macd_signal'] = df['macd'].ewm(span=9, adjust=False).mean()
+        df['macd_hist'] = df['macd'] - df['macd_signal']
+
+    def calculate_bollinger_bands(self, df):
+        """
+        Calculate Bollinger Bands.
+        """
+        sma_20 = df['close'].rolling(window=20).mean()
+        std_20 = df['close'].rolling(window=20).std()
+        df['bollinger_upper'] = sma_20 + (2 * std_20)
+        df['bollinger_lower'] = sma_20 - (2 * std_20)
+
+    def calculate_atr(self, df):
+        """
+        Calculate Average True Range (ATR).
+        """
+        high_low = df['high'] - df['low']
+        high_prev_close = (df['high'] - df['close'].shift()).abs()
+        low_prev_close = (df['low'] - df['close'].shift()).abs()
+        true_range = high_low.to_frame('hl').join(high_prev_close.to_frame('hpc')).join(low_prev_close.to_frame('lpc')).max(axis=1)
+        df['atr_14'] = true_range.rolling(window=14).mean()
+
+    def calculate_volume_features(self, df):
+        """
+        Calculate Volume-Based Features.
+        """
+        df['volume_ma_20'] = df['volume'].rolling(window=20).mean()
+        df['volume_ratio'] = df['volume'] / df['volume_ma_20']
+
+    def calculate_roc(self, df):
+        """
+        Calculate Rate of Change (ROC).
+        """
+        df['roc_10'] = (df['close'] - df['close'].shift(10)) / df['close'].shift(10) * 100
+
+    def calculate_lagged_features(self, df):
+        """
+        Calculate Lagged Features.
+        """
+        df['close_lag_1'] = df['close'].shift(1)
+        df['close_lag_3'] = df['close'].shift(3)
+        df['macd_lag_1'] = df['close'].shift(1)
+
+    def calculate_candle_features(self, df):
+        """
+        Calculate Candle Features.
+        """
+        df['candle_body'] = df['close'] - df['open']
+        df['upper_wick'] = df['high'] - df[['close', 'open']].max(axis=1)
+        df['lower_wick'] = df[['close', 'open']].min(axis=1) - df['low']
--- bot.py
+++ bot.py
@@ -1,12 +1,18 @@
 # Import the class from the Python file (module)
-from BinanceClient import BinanceClient
 import pandas as pd
 import matplotlib.pyplot as plt
 import os
 from dotenv import load_dotenv
 from pathlib import Path
+from sklearn.preprocessing import StandardScaler
+from statsmodels.tsa.seasonal import seasonal_decompose
+import seaborn as sns
+from BinanceClient import BinanceClient
+import numpy as np
+from typing import Final
+import joblib
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 
-fetch_data_from_binance = False
 
 # Initialize Binance client with your API credentials
 dotenv_path = Path('.env-secret')
@@ -14,24 +20,30 @@ load_dotenv(dotenv_path=dotenv_path)
 api_secret = os.getenv("BINANCE_SECRET_KEY")
 api_key = os.getenv("BINANCE_API_KEY")
 
-binance_client = BinanceClient(api_key, api_secret)
+# Create Binance client & initialize it
+binance_client = BinanceClient()
 
+fetch_data_from_binance = False
 # Define trading pair and date range
 pair = "BTCUSDT"
 start_date = "01 Jan, 2024"
-end_date = "01 Dec, 2024"
+end_date = "03 Jan, 2024"
+
 
 if fetch_data_from_binance:
+    # Create connection to fecth data
+    binance_client.make(api_key, api_secret)
+
     # Fetch data
     data = binance_client.fetch_data(pair, start_date, end_date)
     binance_client.store_data_to_db(pair, data)
 else:
     data = binance_client.fetch_data_from_db(pair, start_date, end_date)
-        
-# Check if data is fetched
+
+ # Check if data is fetched
 if not data.empty:
     # Convert the fetched data into a pandas DataFrame
-    df = pd.DataFrame(data, columns=['timestamp', 'open', 'high', 'low', 'close', 'volume', 'close_time', 'quote_asset_volume', 'number_of_trades', 'taker_buy_base_asset_volume', 'taker_buy_quote_asset_volume', 'ignore'])
+    df = pd.DataFrame(data, columns=['timestamp', 'open', 'high', 'low', 'close', 'volume'])
     
     # Convert timestamp to datetime
     df['timestamp'] = pd.to_datetime(df['timestamp'], unit='ms')
@@ -41,9 +53,62 @@ if not data.empty:
     
     # Convert the columns to numeric (some values may be strings by default)
     df[['open', 'high', 'low', 'close', 'volume']] = df[['open', 'high', 'low', 'close', 'volume']].apply(pd.to_numeric)
-
-    # Plot the closing price
-    df['close'].plot(figsize=(10, 6), title=f'{pair} Close Price', xlabel='Date', ylabel='Price (USDT)')
-    plt.show() 
 else:
-    print("No data fetched.")
+    print("No data found!!!.")  
+
+# Define target
+# Predict the percentage price change over the next 'n' candles
+nn = 12         # predict change over 1 hr for 5MIN_INTERVAL
+df['target'] = (df['close'].shift(nn) - df['close']) / df['close'] * 100
+
+# Feature Engineering (mind the order since some features are depended on others)
+binance_client.calculate_sma(df)
+binance_client.calculate_ema(df)
+binance_client.calculate_rsi(df)
+binance_client.calculate_macd(df)
+binance_client.calculate_bollinger_bands(df)
+binance_client.calculate_atr(df)
+binance_client.calculate_volume_features(df)
+binance_client.calculate_roc(df)
+binance_client.calculate_lagged_features(df)
+binance_client.calculate_candle_features(df)
+
+
+## Test/train split
+# drop NaNs
+df.dropna(inplace=True)
+
+# df is sorted by time and indexed by a datetime index
+n = len(df)
+train_end = int(n * 0.8) # 80% data used for training
+train_df = df.iloc[:train_end]
+test_df = df.iloc[train_end:]
+
+# Separate features and target
+X_train = train_df.drop(['target', 'close'], axis=1)
+y_train = train_df['target']
+
+X_test = test_df.drop(['target', 'close'], axis=1)
+y_test = test_df['target']
+
+# Load the best model
+best_model = joblib.load('best_model_12candles.joblib')
+
+# Generate predictions
+predictions = pd.DataFrame(best_model.predict(X_test))  # Predicted percentage changes
+
+# Define buy & sell thrsholds
+buy_threshold = -0.37999999999999856
+sell_threshold = 0.29000000000000004
+
+# Generate trade signals
+# Generate signals with reversed logic
+trading_signals = [
+    "Buy" if pred < buy_threshold else "Sell" if pred > sell_threshold else "Hold"
+    for pred in results_df['Predicted Change']
+]
+
+
+
+
+
